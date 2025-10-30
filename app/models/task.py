d3793c9128fdc8089807a8e3ff6fc27eb645935d
Riley Drellishak <rileydrellishak@gmail.com>
2025-10-30 17:22:41 -0400

    Completes attributes for Task model

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,10 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from typing import Optional
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
