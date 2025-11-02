7411f0a248c91fcbad65185e77cc2f3ef8a40e98
Riley Drellishak <rileydrellishak@gmail.com>
2025-11-02 15:07:14 -0500

    Establishes one to many relationship for goal to tasks, migration included

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
 from typing import Optional
 from datetime import datetime
@@ -6,6 +6,7 @@ from datetime import datetime
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
+    tasks: Mapped[list['Task']] = relationship(back_populates='goal')
 
     @classmethod
     def from_dict(cls, goal_data):
