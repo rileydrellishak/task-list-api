ab5576382ea755f3f3accfbc90244ad370abf553
Riley Drellishak <rileydrellishak@gmail.com>
2025-11-04 15:35:58 -0500

    Adds tasks attribute to Goal, adds goal_id and goal attributes to Task

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
 from typing import Optional
 from datetime import datetime
@@ -6,19 +6,15 @@ from datetime import datetime
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
+    tasks: Mapped[list["Task"]] = relationship(back_populates="goal")
 
     @classmethod
     def from_dict(cls, goal_data):
-        new_goal = Goal(
-            title=goal_data['title']
-            )
-        
+        new_goal = cls(title=goal_data['title'])
         return new_goal
     
     def to_dict(self):
-        goal_dict = {}
-        
-        goal_dict['id'] = self.id
-        goal_dict['title'] = self.title
-
-        return goal_dict
+        return {
+            'id': self.id, 
+            'title': self.title
+            }
