6adb2dd00b95f1c4441404021946e30b93854bdf
Riley Drellishak <rileydrellishak@gmail.com>
2025-11-04 15:40:27 -0500

    Registers goal routes bp

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .db import db, migrate
 from .models import task, goal
 from .routes.task_routes import bp as tasks_bp
+from .routes.goal_routes import bp as goals_bp
 import os
 
 def create_app(config=None):
@@ -11,14 +12,12 @@ def create_app(config=None):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
     if config:
-        # Merge `config` into the app's configuration
-        # to override the app's default settings for testing
         app.config.update(config)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Register Blueprints here
     app.register_blueprint(tasks_bp)
+    app.register_blueprint(goals_bp)
 
     return app
